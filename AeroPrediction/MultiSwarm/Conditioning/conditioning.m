8af3766cff96d9c033a51ebbf025dafcd24531bb
Acusick1 <a.cusick.1@research.gla.ac.uk>
2018-11-15 22:32:46 +0000

    Remove duplicate folders accidentally pushed in previous commit

--- AeroPrediction/MultiSwarm/Conditioning/conditioning.m
+++
@@ -1,9 +0,0 @@
-function [parPos,physicalPos] = conditioning(parPos,cond,varArray,options)
-
-hardstyle = options.hardconditioning;
-
-if hardstyle
-    [parPos,physicalPos] = hardcon(parPos,cond,varArray);
-else
-    [parPos,physicalPos] = versatilecon(parPos,cond,varArray);
-end
